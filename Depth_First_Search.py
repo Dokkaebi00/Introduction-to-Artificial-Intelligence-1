6796f52476cfe0dcfbc03b381503978a12b76d41
VinhDevNguyen <vinh2000ld14@gmail.com>
2020-03-17 16:32:05 +0700

    Update Depth First Search

--- Depth_First_Search.py
+++ Depth_First_Search.py
@@ -1,4 +1,7 @@
 # %% [markdown]
+# # The Algorithm
+# 1. Pick any node. If it is unvisited, mark it as visited and recur on all its adjacent nodes.
+# 2. Repeat until all the nodes are visited, or the node to be searched is found.
 # # Create Graph
 # We have to create graph like this: 
 # ![alt text](https://www.educative.io/api/edpresso/shot/5410617873661952/image/6437799702036480 "Graph")
